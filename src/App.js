46d895f61d7fd06ae163437d78d78371ca1e23f7
ricardo <ricardo@email.com>
2022-12-12 21:31:45 -0500

    la hora en react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom/client';
 
 function App() {
-  return (
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+    const element = (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <h1>La hora es:</h1>
+    <h2>Son las {new Date().toLocaleTimeString()}</h2>
       </header>
     </div>
-  );
+    );
+    root.render(element)
 }
 
 export default App;
